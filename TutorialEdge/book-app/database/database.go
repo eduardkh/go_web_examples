e5b5e2906ecba52bd470bd282fff0bb7aefb10e5
Eduard Khiaev <eddietheone1984@gmail.com>
2020-12-07 21:08:52 +0200

    add db

---
+++ TutorialEdge/book-app/database/database.go
@@ -0,0 +1,9 @@
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var (
+	DBConn *gorm.DB
+)
--- TutorialEdge/book-app/main.go
+++ TutorialEdge/book-app/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/eduardkh/go_web_examples/TutorialEdge/book-app/book"
+	"github.com/eduardkh/go_web_examples/TutorialEdge/book-app/database"
 
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func helloWorld(c *fiber.Ctx) {
@@ -20,9 +25,20 @@ func setupRoutes(app *fiber.App) {
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
+
 func main() {
 	app := fiber.New()
 
 	setupRoutes(app)
 	app.Listen(3000)
+
+	defer database.DBConn.Close()
 }
