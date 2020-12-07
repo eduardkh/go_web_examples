ee47945b15442dc59bcc719eb251add00a3bab40
Eduard Khiaev <eddietheone1984@gmail.com>
2020-12-07 20:42:20 +0200

    book package

--- TutorialEdge/Building a Go REST API using Gorm and Fiber!/main.go
+++ TutorialEdge/Building a Go REST API using Gorm and Fiber!/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/eduardkh/go_web_examples/tree/main/TutorialEdge/Building%20a%20Go%20REST%20API%20using%20Gorm%20and%20Fiber!/book"
 	"github.com/gofiber/fiber"
 )
 
@@ -10,6 +11,12 @@ func helloWorld(c *fiber.Ctx) {
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/", helloWorld)
+
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Put("/api/v1/book", book.UpdateBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
 func main() {
